57eeb323931845ee7508e4f244483038437332ad
Aleksandar Misovic <aleksandar.misovic@fisglobal.com>
2021-03-14 15:39:48 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const os = require('os');
+
+console.log("Kubia server starting...");
+
+var requestCount = 0;
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  requestCount++;
+  response.writeHead(200);
+  response.write("VERSION: 1 " + "\n");
+  response.write("Request Count: " + requestCount + "\n");
+  response.write("Request comes from IP: " + request.connection.remoteAddress + "\n");
+  response.end("You've hit " + os.hostname() + "\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
+
